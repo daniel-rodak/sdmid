c7925a049b2fb88efc15efd6c8027d3820b3b950
daniel-rodak <daniel.rodak1@gmail.com>
2018-02-20 16:03:53 +0100

    Add spinner, code refactoring

--- sound_tembre/app.R
+++ sound_tembre/app.R
@@ -6,6 +6,22 @@ library(SynchWave)
 library(plotly)
 library(dplyr)
 
+spinner <- function(spinner.path) {
+  if (isTruthy(spinner.path)) {
+    conditionalPanel(
+      condition = "($('html').hasClass('shiny-busy'))",
+      tags$div(id = "spinnerLoading",
+               style = "margin: 0px; padding: 0px;
+               position: fixed; right: 0px; top: 0px;
+               width: 100%; height: 100%; z-index:30001; opacity: 0.8;",
+               tags$p(style = "position: absolute; top: 50%; left: 45%;",
+                      tags$img(src = spinner.path)))
+    )
+  } else {
+    NULL
+  }
+}
+
 hamming <- function(N, a = 0.53836, b = 0.46164) {
   a - b * cos(2*pi*0:(N-1) / (N - 1))
 }
@@ -13,10 +29,6 @@ hamming <- function(N, a = 0.53836, b = 0.46164) {
 getNote <- function(frq, snd) {
   closest.freq <- which.min(abs(snd$Frequency - frq))
   return(snd[closest.freq, "Note"])
-}
-
-getMaxima <- function(x) {
-  which(diff(sign(diff(x)))==-2)+1
 }
 
 minMaxNormalize <- function(x) {
@@ -33,13 +45,12 @@ makeDfPlot <- function(wav, snd, bck = "sound/bck_voice.wav") {
   
   df.plot <- data.frame(Frequency = dF*1:round(N/2),
                         Amplitude = abs((fft_wav[1:round(N/2)])^2))
-  df.plot$Smoothed <- minMaxNormalize(as.numeric(smooth::sma(df.plot$Amplitude, order = 16)$fitted))
+  df.plot$Smoothed <- minMaxNormalize(as.numeric(smooth::sma(df.plot$Amplitude, order = 8)$fitted))
   df.plot$Note <- sapply(df.plot$Frequency, getNote, snd)
   return(df.plot)
 }
   
 makePlot <- function(df.plot, tit = "Widmo") {
-  # df.plot <- df.plot[getMaxima(df.plot$Smoothed), ]
   p <- plot_ly(data = df.plot, x = ~Frequency, y = ~Smoothed,
                type = 'scatter', mode = 'lines',
                text = ~paste("Freq:", round(Frequency), "Hz<br>Note:", Note))
@@ -62,8 +73,8 @@ makeCompPlot <- function(wav1, wav2, snd, tit = "Widmo", bck = "sound/bck_voice.
   df.plot <- data.frame(Frequency = dF*1:round(N/2),
                         Amplitude1 = abs((fft_wav1[1:round(N/2)])^2),
                         Amplitude2 = abs((fft_wav2[1:round(N/2)])^2))
-  df.plot$Smoothed1 <- minMaxNormalize(as.numeric(smooth::sma(df.plot$Amplitude1, order = 16)$fitted))
-  df.plot$Smoothed2 <- minMaxNormalize(as.numeric(smooth::sma(df.plot$Amplitude2, order = 16)$fitted))
+  df.plot$Smoothed1 <- minMaxNormalize(as.numeric(smooth::sma(df.plot$Amplitude1, order = 8)$fitted))
+  df.plot$Smoothed2 <- minMaxNormalize(as.numeric(smooth::sma(df.plot$Amplitude2, order = 8)$fitted))
   df.plot$Note <- sapply(df.plot$Frequency, getNote, snd)
   
   p <- plot_ly(data = df.plot, x = ~Frequency, y = ~Smoothed1, name = "Pierwszy dźwięk",
@@ -78,31 +89,66 @@ makeCompPlot <- function(wav1, wav2, snd, tit = "Widmo", bck = "sound/bck_voice.
 
 snd <- read.csv("sound_list.csv", stringsAsFactors = FALSE)
 smp <- read.csv("sample_list.csv", stringsAsFactors = FALSE)
-# p <- makePlot(hmn_Dnl_A, snd, tit = "Widmo dźwięku A, Daniel")
-# p
+selPntTemplate <- data.frame(Note = character(0), Frequency = numeric(0), Smoothed = numeric(0))
+selectedDfTemplate <- function() {
+  ret <- data.frame(Note = character(0),
+                    Frequency = numeric(0),
+                    Smoothed = numeric(0),
+                    RelAmp = numeric(0))
+  colnames(ret) <- colnames(ret) <- c('Nuta', 'Częstotliwość', 'Amplituda', 'Amplituda względna [dB]')
+  ret
+}
+soundCompFrameTemplate <- data.frame(
+  SoundID = character(),
+  Source = character(),
+  Desc = character(),
+  Frequency = numeric(),
+  RelAmp = numeric()
+)
 
 ui <- dashboardPage(
   dashboardHeader(title = "Analiza barwy dźwięku"),
   dashboardSidebar(
-    sidebarMenu(
+    sidebarMenu(id = 'menu1',
       menuItem("Widmo", tabName = "spec", icon = icon("signal")),
       menuItem("Porównaj", tabName = "comp", icon = icon("object-group")),
       menuItem("Charakterystyka", tabName = "char", icon = icon("braille"))
-    )
+    ),
+    conditionalPanel(
+      condition = "input.menu1 == 'spec'",
+      selectInput('src', 'Źródło dźwięku', choices = unique(smp$source)),
+      selectInput('dsc', 'Rodzaj dźwięku', choices = unique(smp$snd_desc)),
+      selectInput('ptc', 'Wysokość dźwięku', choices = unique(smp$pitch)),
+      actionButton('btn', 'Narysuj wykres')
+    ),
+    conditionalPanel(
+      condition = "input.menu1 == 'comp'",
+      menuItem("Pierwszy dźwięk", startExpanded = TRUE,
+        selectInput('src1', 'Źródło dźwięku', choices = unique(smp$source)),
+        selectInput('dsc1', 'Rodzaj dźwięku', choices = unique(smp$snd_desc)),
+        selectInput('ptc1', 'Wysokość dźwięku', choices = unique(smp$pitch))
+      ),
+      menuItem('Drugi dźwięk', startExpanded = TRUE,
+        selectInput('src2', 'Źródło dźwięku', choices = unique(smp$source)),
+        selectInput('dsc2', 'Rodzaj dźwięku', choices = unique(smp$snd_desc)),
+        selectInput('ptc2', 'Wysokość dźwięku', choices = unique(smp$pitch))
+      ),
+      actionButton('btn2', 'Narysuj wykres')
+    ),
+    tags$div(id = 'spinnerAttr',
+             style = 'position: fixed; bottom: 0px; margin: 1%;',
+             tags$p(tags$a('spinner by loading.io', href = 'https://loading.io/')))
   ),
   
   dashboardBody(
     tabItems(
       tabItem(tabName = 'spec',
+        spinner('Ellipsis.svg'),
         fluidRow(
-          box(width = 3,
-            selectInput('src', 'Źródło dźwięku', choices = unique(smp$source)),
-            selectInput('dsc', 'Rodzaj dźwięku', choices = unique(smp$snd_desc)),
-            selectInput('ptc', 'Wysokość dźwięku', choices = unique(smp$pitch)),
-            actionButton('btn', 'Narysuj wykres')
-          ),
-          box(width = 9,
-            plotlyOutput('plt'),
+          box(width = 12, plotlyOutput('plt'))
+        ),
+        fluidRow(
+          box(width = 12, title = "Tabelka z zaznaczonymi pikami",
             DT::dataTableOutput('tbl'),
             actionButton('rst', 'Resetuj tabelkę'),
             actionButton('add', 'Dodaj do charakterystyki')
@@ -111,27 +157,14 @@ ui <- dashboardPage(
       ),
       
       tabItem(tabName = 'comp',
+        spinner('Ellipsis.svg'),
         fluidRow(
-          column(width = 3,
-            box(width = NULL, title = 'Pierwszy dźwięk',
-              selectInput('src1', 'Źródło dźwięku', choices = unique(smp$source)),
-              selectInput('dsc1', 'Rodzaj dźwięku', choices = unique(smp$snd_desc)),
-              selectInput('ptc1', 'Wysokość dźwięku', choices = unique(smp$pitch))
-            ),
-            box(width = NULL, title = 'Drugi dźwięk',
-              selectInput('src2', 'Źródło dźwięku', choices = unique(smp$source)),
-              selectInput('dsc2', 'Rodzaj dźwięku', choices = unique(smp$snd_desc)),
-              selectInput('ptc2', 'Wysokość dźwięku', choices = unique(smp$pitch)),
-              actionButton('btn2', 'Narysuj wykres')
-            )
-          ),
-          column(width = 9,
-            box(width = NULL, plotlyOutput('plt2'))
-          )
+          box(width = 12, plotlyOutput('plt2'))
         )
       ),
       
       tabItem(tabName = 'char',
+        spinner('Ellipsis.svg'),
         fluidRow(
           box(width = 12, title = 'Charakterystyka wszystkich dźwięków',
             plotlyOutput('plt3')
@@ -147,7 +180,8 @@ ui <- dashboardPage(
         ),
         fluidRow(
           box(width = 12, title = "Tabela z danymi:",
-            DT::dataTableOutput('tbl2')
+            DT::dataTableOutput('tbl2'),
+            actionButton('rst2', 'Resetuj tabelkę')
           )
         )
       )
@@ -156,28 +190,34 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output, session) {
+
+# spec tab ----------------------------------------------------------------
+
   wav <- reactive(readWave(smp$path[smp$source == input$src & smp$snd_desc == input$dsc & smp$pitch == input$ptc]))
-  tit <- reactive(sprintf("Widmo dźwięku: %s, o źródle: %s", input$dsc, input$src))
-  selPnt <- reactiveVal(data.frame(Note = character(0),
-                                   Frequency = numeric(0),
-                                   Smoothed = numeric(0)))
-  selectedDf <- reactiveVal()
-  soundCompFrame <- reactiveVal(data.frame(SoundID = character(),
-                                           Source = character(),
-                                           Desc = character(),
-                                           Frequency = numeric(),
-                                           RelAmp = numeric()))
+  selPnt <- reactiveVal(selPntTemplate)
+  selectedDf <- reactiveVal(selectedDfTemplate())
+  soundCompFrame <- reactiveVal(soundCompFrameTemplate)
   
   dfPlot <- eventReactive(input$btn, {
     makeDfPlot(wav(), snd)
   })
   observeEvent(input$src, {
+    dscCache <- input$dsc
+    chcs <- smp[smp$source == input$src, 'snd_desc']
+    if (!(dscCache %in% chcs)) {
+      dscCache <- chcs[1]
+    }
     updateSelectInput(session, 'dsc',
-                      choices = smp[smp$source == input$src, 'snd_desc'])
+                      choices = smp[smp$source == input$src, 'snd_desc'],
+                      selected = dscCache)
   })
   observeEvent(input$btn, {
     req(dfPlot())
-    output$plt <- renderPlotly(makePlot(dfPlot(), tit()))
+    tit <- sprintf("Widmo dźwięku: %s, o źródle: %s", input$dsc, input$src)
+    output$plt <- renderPlotly(makePlot(dfPlot(), tit))
+    # Reset
+    selPnt(selPntTemplate)
+    selectedDf(selectedDfTemplate())
   })
   observeEvent(event_data('plotly_click'), { 
     req(dfPlot())
@@ -192,17 +232,12 @@ server <- function(input, output, session) {
     ret$RelAmp <- round(10*log10(ret$Smoothed / ret$Smoothed[1]), 3)
     ret$Smoothed <- round((ret$Smoothed), 3)
     ret$Frequency <- round(ret$Frequency)
-    colnames(ret) <- c('Nuta', 'Częstotliwość', 'Amplituda', 'Amplituda względna dB')
+    colnames(ret) <- c('Nuta', 'Częstotliwość', 'Amplituda', 'Amplituda względna [dB]')
     selectedDf(ret)
   })
   observeEvent(input$rst, {
-    selPnt(data.frame(Note = character(0),
-                      Frequency = numeric(0),
-                      Smoothed = numeric(0)))
-    ret <- selPnt()
-    ret$RelAmp <- numeric(0)
-    colnames(ret) <- c('Nuta', 'Częstotliwość', 'Amplituda (log)', 'Amplituda względna (log/log)')
-    selectedDf(ret)
+    selPnt(selPntTemplate)
+    selectedDf(selectedDfTemplate())
   })
   output$tbl <- DT::renderDataTable({
     req(selectedDf())
@@ -217,24 +252,58 @@ server <- function(input, output, session) {
     ret$Desc <- input$dsc
     soundCompFrame(rbind(soundCompFrame(), ret[, c('SoundID', 'Source', 'Desc', 'Frequency', 'RelAmp')]))
     # Reset
-    selPnt(data.frame(Note = character(0),
-                      Frequency = numeric(0),
-                      Smoothed = numeric(0)))
-    ret <- selPnt()
-    ret$RelAmp <- numeric(0)
-    colnames(ret) <- c('Nuta', 'Częstotliwość', 'Amplituda (log)', 'Amplituda względna (log/log)')
-    selectedDf(ret)
+    selPnt(selPntTemplate)
+    selectedDf(selectedDfTemplate())
   })
+
+# comp tab ----------------------------------------------------------------
+
+  observeEvent(input$src1, {
+    dscCache <- input$dsc1
+    chcs <- smp[smp$source == input$src1, 'snd_desc']
+    if (!(dscCache %in% chcs)) {
+      dscCache <- chcs[1]
+    }
+    updateSelectInput(session, 'dsc1',
+                      choices = smp[smp$source == input$src, 'snd_desc'],
+                      selected = dscCache)
+  })
+  observeEvent(input$src2, {
+    dscCache <- input$dsc2
+    chcs <- smp[smp$source == input$src2, 'snd_desc']
+    if (!(dscCache %in% chcs)) {
+      dscCache <- chcs[1]
+    }
+    updateSelectInput(session, 'dsc2',
+                      choices = smp[smp$source == input$src2, 'snd_desc'],
+                      selected = dscCache)
+  })
+  observeEvent(input$btn2, {
+    wav1 <- readWave(smp$path[smp$source == input$src1 & smp$snd_desc == input$dsc1 & smp$pitch == input$ptc1])
+    wav2 <- readWave(smp$path[smp$source == input$src2 & smp$snd_desc == input$dsc2 & smp$pitch == input$ptc2])
+    tit2 <- "Porównanie widm dźwięków"
+    output$plt2 <- renderPlotly(makeCompPlot(wav1, wav2, snd, tit2))
+  })
+
+# char tab ----------------------------------------------------------------
+
   output$tbl2 <- DT::renderDataTable({
     req(soundCompFrame())
-    soundCompFrame()
+    ret <- soundCompFrame()
+    ret$Frequency <- round(ret$Frequency)
+    ret$RelAmp <- round(ret$RelAmp, 3)
+    colnames(ret) <- c('ID dźwięku', 'Źródło', 'Rodzaj', 'Częstotliwość', 'Amplituda względna [dB]')
+    ret
+  })
+  observeEvent(input$rst2, {
+    soundCompFrame(soundCompFrameTemplate)
   })
   output$plt3 <- renderPlotly({
     req(soundCompFrame())
     p <- plot_ly(data = soundCompFrame(), x = ~Frequency, y = ~RelAmp, color = ~SoundID)
     p <- add_lines(p)
     p <- layout(p, xaxis = list(type = 'log', range = c(log10(200), log10(10000)), title = "Częstotliwość [Hz]"),
-                yaxis = list(title = 'Amplituda względna'))
+                yaxis = list(title = 'Amplituda względna [dB]'))
     p
   })
   output$pltdsc <- renderPlotly({
@@ -248,7 +317,7 @@ server <- function(input, output, session) {
     p <- plot_ly(data = dfr, x = ~Frequency, y = ~RelAmp, color = ~Desc)
     p <- add_lines(p)
     p <- layout(p, xaxis = list(type = 'log', range = c(log10(200), log10(10000)), title = "Częstotliwość [Hz]"),
-                yaxis = list(title = 'Amplituda względna'))
+                yaxis = list(title = 'Amplituda względna [dB]'))
     p
   })
   output$pltsrc <- renderPlotly({
@@ -262,24 +331,8 @@ server <- function(input, output, session) {
     p <- plot_ly(data = dfr, x = ~Frequency, y = ~RelAmp, color = ~Source)
     p <- add_lines(p)
     p <- layout(p, xaxis = list(type = 'log', range = c(log10(200), log10(10000)), title = "Częstotliwość [Hz]"),
-                yaxis = list(title = 'Amplituda względna'))
+                yaxis = list(title = 'Amplituda względna [dB]'))
     p
-  })
-  
-  wav1 <- reactive(readWave(smp$path[smp$source == input$src1 & smp$snd_desc == input$dsc1 & smp$pitch == input$ptc1]))
-  wav2 <- reactive(readWave(smp$path[smp$source == input$src2 & smp$snd_desc == input$dsc2 & smp$pitch == input$ptc2]))
-  tit2 <- "Porównanie widm dźwięków"
-  observeEvent(input$src1, {
-    updateSelectInput(session, 'dsc1',
-                      choices = smp[smp$source == input$src1, 'snd_desc'])
-  })
-  observeEvent(input$src2, {
-    updateSelectInput(session, 'dsc2',
-                      choices = smp[smp$source == input$src2, 'snd_desc'])
-  })
-  
-  observeEvent(input$btn2, {
-    output$plt2 <- renderPlotly(makeCompPlot(wav1(), wav2(), snd, tit2))
   })
 }
 
